ec228d62a306f7789c8f8bbc15fe45457723fe40
Codewiizard <135721204+Codewiizard@users.noreply.github.com>
2026-02-13 12:53:44 +0530

    set openai apikey

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,12 @@
+import { configDotenv } from "dotenv";
 import express from "express";
 import type { Request,Response } from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+configDotenv();
 app.get('/',(req:Request,res:Response)=>{
-    res.send('Hello World!')
+    res.send("Hello World!")
 });
 app.listen(port,()=>{
     console.log(`Server is runnning on http://localhost:${port}`)
