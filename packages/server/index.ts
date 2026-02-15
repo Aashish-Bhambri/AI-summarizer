cc9b353970d206ca42745d706d1889df163e721d
Codewiizard <135721204+Codewiizard@users.noreply.github.com>
2026-02-15 22:46:57 +0530

    add OpenAI dependency and implement chat endpoint in server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,16 +1,43 @@
 import { configDotenv } from 'dotenv';
 import express from 'express';
 import type { Request, Response } from 'express';
+import OpenAI from 'openai';
+
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 configDotenv();
+
+const client = new OpenAI({
+   apiKey:process.env.OPENAI_API_KEY
+})
+
 app.get('/', (req: Request, res: Response) => {
    res.send('Hello World!');
 });
 app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World!' });
 });
+
+const conversations = new Map<string,string>();
+
+app.post('/api/chat',async(req:Request, res:Response)=>{
+   
+   const {prompt,conversationID}=req.body;
+   const response= await client.responses.create({
+      model:'gpt-4o-mini',
+      input:prompt,
+      temperature:0.2,
+      max_output_tokens:100,
+      previous_response_id:conversations.get(conversationID)
+   })
+
+   conversations.set(conversationID,response.id);
+   res.json({message: response.output_text})
+   
+})
 app.listen(port, () => {
    console.log(`Server is runnning on http://localhost:${port}`);
 });
