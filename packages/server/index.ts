e2d5fb8449a8c02523aba6400e15dc8818ff4d2e
Codewiizard <135721204+Codewiizard@users.noreply.github.com>
2026-03-16 10:19:07 +0530

    implement chat functionality with request validation and OpenAI integration

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,67 +1,19 @@
 import { configDotenv } from 'dotenv';
 import express from 'express';
 import type { Request, Response } from 'express';
-import OpenAI from 'openai';
-import z from 'zod';
-import { conversationRepository } from './repositories/conversation.repository';
+import { chatController } from './controllers/chat.controller';
+import router from './routes';
 
 
 const app = express();
 app.use(express.json());
+app.use(router);
 
 const port = process.env.PORT || 3000;
 
 configDotenv();
 
-const client = new OpenAI({
-   apiKey:process.env.OPENAI_API_KEY
-})
-
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello World!');
-});
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World!' });
-});
-
-
-
-const chatSchema = z.object({
-   prompt:z.string()
-   .trim()
-   .min(1,'Prompt is Required')
-   .max(1000,"Prompt is too long (max 1000 characters)"),
-   conversationID:z.string().uuid()
-})
-
-app.post('/api/chat',async(req:Request, res:Response)=>{
-   const parseResult = chatSchema.safeParse(req.body);
-
-   if(!parseResult.success){
-      res.status(400).json(parseResult.error.format());
-      return;
-   }
-
- 
-
-   try{
-   const {prompt,conversationID}=req.body;
-   const response= await client.responses.create({
-      model:'gpt-4o-mini',
-      input:prompt,
-      temperature:0.2,
-      max_output_tokens:100,
-      previous_response_id:conversationRepository.getLastResponseId(conversationID),
-   })
-
-   conversationRepository.setlastResponseId(conversationID,response.id)
-   res.json({message: response.output_text})
-
-   }catch(error){
-      res.status(500).json({error:"Failed to generate response"})
-   }
-})
-
 app.listen(port, () => {
    console.log(`Server is runnning on http://localhost:${port}`);
 });
+ 
